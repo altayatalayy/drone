f525b2ad901f2aff0b76be92964f83fb47d05d1c
altayatalayy <altayatalay1998@gmail.com>
2020-12-06 14:35:09 +0000

    updated app/__init__.py

--- app/flask/app/__init__.py
+++ app/flask/app/__init__.py
@@ -3,28 +3,30 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
 
-from app.config import DevConfig, TestConfig
+from app.config import ProdConfig, DevConfig, TestConfig
+
+import redis
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object(DevConfig)
+    app.config.from_object(ProdConfig)
 
     db = SQLAlchemy(app)
     bcrypt = Bcrypt(app)
     login_manager = LoginManager(app)
     login_manager.login_view = 'main.login'
+    r = redis.Redis('localhost')
 
-    return app, db, bcrypt, login_manager
+    return app, db, bcrypt, login_manager, r
 
-app, db, bcrypt, login_manager = create_app()
+app, db, bcrypt, login_manager, r = create_app()
 
-if __name__ == 'app':
-    from app.main.routes import main
-    from app.apis.data import data
-    from app.apis.auth import auth
-    app.register_blueprint(main)
-    app.register_blueprint(data)
-    app.register_blueprint(auth)
+from app.main.routes import main
+from app.apis.data import data
+from app.apis.auth import auth
+app.register_blueprint(main)
+app.register_blueprint(data)
+app.register_blueprint(auth)
 
 
 
